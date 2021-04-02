1bdb9f51faffd4b650a4b4ce4fea9d75a0ee0d11
David PETIPAS <david.petipas@gmail.com>
2021-04-02 19:33:53 +0200

    Initial Commit

---
+++ Shared/lesBasiquesApp.swift
@@ -0,0 +1,17 @@
+//
+//  lesBasiquesApp.swift
+//  Shared
+//
+//  Created by David PETIPAS on 02/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct lesBasiquesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
